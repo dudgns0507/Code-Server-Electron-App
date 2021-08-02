25621b5ff9284eac2fea83e823dffb5dbd92c0a9
copin-danny <hoon2585@gmail.com>
2021-08-02 22:25:09 +0900

    Fix close error

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
 const { app, Menu, BrowserWindow, globalShortcut } = require("electron");
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+var path = require('path')
 let mainWindow;
 
 function createWindow() {
-  // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 1280,
-    height: 720,
+    width: 1600,
+    height: 900,
     fullscreen: false,
     webPreferences: {
-      nodeIntegration: true
-    }
+        nodeIntegration: true
+    },
+    icon: path.join(__dirname, 'default_dark.png')
   });
 
   app.on('browser-window-focus', () => {
@@ -29,21 +27,17 @@ function createWindow() {
       globalShortcut.unregister('F5');
   });
 
-  // and load the index.html of the app.
-  mainWindow.loadURL("https://example.com/");
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
+  mainWindow.loadURL("https://code.cropo.me/");
+  
   mainWindow.on("closed", function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    Electron.session.defaultSession.clearCache(() => {})
+    try {
+      Electron.session.defaultSession.clearCache(() => {})
+    } catch {
+
+    }
     mainWindow = null;
   });
-  // Create the Application's main menu
+
   const template = [
     {
       label: "Application",
@@ -83,23 +77,15 @@ function createWindow() {
   Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on("ready", createWindow);
 
-// Quit when all windows are closed.
 app.on("window-all-closed", function() {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
 app.on("activate", function() {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
     createWindow();
   }
@@ -108,8 +94,6 @@ app.on("activate", function() {
 app.on(
   "certificate-error",
   (event, webContents, url, error, certificate, callback) => {
-    // On certificate error we disable default behaviour (stop loading the page)
-    // and we then say "it is all fine - true" to the callback
     event.preventDefault();
     callback(true);
   }
